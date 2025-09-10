c7d9f41135ce97bee2d0541f3b9675cee8f0d2e1
A-Lana-23 <128288677+A-Lana-23@users.noreply.github.com>
2025-09-10 23:00:20 +0700

    Delete autoexpert-site directory

--- autoexpert-site/js/script.js
+++
@@ -1,6 +0,0 @@
-// Простой скрипт для отправки формы (можно расширить)
-document.querySelector('.contact-form')?.addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Спасибо! Я свяжусь с вами в ближайшее время.');
-  this.reset();
-});
